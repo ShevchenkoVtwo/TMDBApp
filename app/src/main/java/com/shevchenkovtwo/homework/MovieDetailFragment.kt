7c1b456408592555bd47f4596d0c14e43f5f79aa
vladimir4999 <shevchenkovtwo@gmail.com>
2020-12-05 12:24:44 +0300

    Add to fragments recyclerview

--- app/src/main/java/com/shevchenkovtwo/homework/MovieDetailFragment.kt
+++ app/src/main/java/com/shevchenkovtwo/homework/MovieDetailFragment.kt
@@ -5,24 +5,62 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.snackbar.Snackbar
+import com.shevchenkovtwo.homework.AppConstants.selectedMovie
 import com.shevchenkovtwo.homework.databinding.FragmentMovieDetailBinding
 
 
 class MovieDetailFragment : Fragment() {
 
     private var fragmentMovieDetailBinding: FragmentMovieDetailBinding? = null
+    private lateinit var recyclerView: RecyclerView
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
         val binding = FragmentMovieDetailBinding.inflate(inflater, container, false)
         fragmentMovieDetailBinding = binding
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        fragmentMovieDetailBinding?.back?.setOnClickListener {
-            findNavController().navigate(R.id.moviesListFragment)
+        fragmentMovieDetailBinding?.let {
+            it.apply {
+                movieName.text = selectedMovie?.name
+                movieRating.rating = selectedMovie!!.rating
+                selectedMovie!!.storyline?.let { storyline -> movieStoryline.setText(storyline) }
+                pg.text = selectedMovie?.pg
+                reviews.text = selectedMovie?.reviews
+                tag.text = selectedMovie?.tags
+                selectedMovie!!.imageMask?.let { image -> mask.setImageResource(image) }
+                if (selectedMovie!!.favorite) {
+                    favourite.setImageResource(R.drawable.ic_favorite)
+                } else {
+                    favourite.setImageResource(R.drawable.ic_not_favorite)
+                }
+                recyclerView = actors
+                recyclerView.adapter =
+                    selectedMovie!!.actors?.let { actors -> ActorsAdapter(actors) }
+                recyclerView.layoutManager =
+                    LinearLayoutManager(requireContext(), RecyclerView.HORIZONTAL, false)
+                back.setOnClickListener {
+                    findNavController().navigate(R.id.moviesListFragment)
+                }
+            }
         }
+        Snackbar.make(
+            view,
+            "If you see not full info about movie, it will be updated soon!",
+            Snackbar.LENGTH_LONG
+        )
+            .setTextColor(ContextCompat.getColor(requireContext(), R.color.title_color))
+            .show()
         super.onViewCreated(view, savedInstanceState)
     }
 
--- app/src/main/java/com/shevchenkovtwo/homework/MoviesListFragment.kt
+++ app/src/main/java/com/shevchenkovtwo/homework/MoviesListFragment.kt
@@ -1,30 +1,40 @@
 package com.shevchenkovtwo.homework
 
+import android.content.res.Configuration
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.navigation.findNavController
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.shevchenkovtwo.homework.databinding.FragmentMoviesListBinding
 
 
 class MoviesListFragment : Fragment() {
 
     private var fragmentMoviesListBinding: FragmentMoviesListBinding? = null
+    private lateinit var recyclerView: RecyclerView
+    private var spanCount: Int = 2
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
         val binding = FragmentMoviesListBinding.inflate(inflater, container, false)
         fragmentMoviesListBinding = binding
+        spanCount = if (resources.configuration.orientation == Configuration.ORIENTATION_PORTRAIT)
+            AppConstants.ORIENTATION_VERTICAL else AppConstants.ORIENTATION_HORIZONTAL
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        fragmentMoviesListBinding?.let { it ->
-            it.movieLayout.root.setOnClickListener { view ->
-                view.findNavController().navigate(R.id.movieDetailFragment)
-            }
-        }
+        val movies = DataSource().getMovies()
+        recyclerView = fragmentMoviesListBinding!!.moviesList
+        recyclerView.adapter = MoviesAdapter(movies)
+        recyclerView.layoutManager =
+            GridLayoutManager(requireContext(), spanCount, RecyclerView.VERTICAL, false)
         super.onViewCreated(view, savedInstanceState)
     }
 
