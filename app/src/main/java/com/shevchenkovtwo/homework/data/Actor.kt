ac16002f1b3f42bd6b75d273048bc2bface19e52
vladimir4999 <shevchenkovtwo@gmail.com>
2020-12-23 15:29:45 +0300

    Add models and json data

---
+++ app/src/main/java/com/shevchenkovtwo/homework/data/Actor.kt
@@ -0,0 +1,7 @@
+package com.shevchenkovtwo.homework.data
+
+data class Actor(
+    val id: Int,
+    val name: String,
+    val picture: String
+)
---
+++ app/src/main/java/com/shevchenkovtwo/homework/data/Genre.kt
@@ -0,0 +1,3 @@
+package com.shevchenkovtwo.homework.data
+
+data class Genre(val id: Int, val name: String)
