4f822896165633a326bfcf276a0dce786d2efaa8
vladimir4999 <shevchenkovtwo@gmail.com>
2020-12-27 16:18:57 +0300

    Rework ViewModel

---
+++ app/src/main/java/com/shevchenkovtwo/homework/ui/viewmodels/MoviesViewModel.kt
@@ -0,0 +1,30 @@
+package com.shevchenkovtwo.homework.ui.viewmodels
+
+import android.app.Application
+import android.os.Bundle
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.viewModelScope
+import com.shevchenkovtwo.homework.data.Movie
+import com.shevchenkovtwo.homework.data.loadMovies
+import com.shevchenkovtwo.homework.ui.adapters.MoviesAdapter.Companion.selectedMovie
+import kotlinx.coroutines.launch
+
+
+class MoviesViewModel(application: Application) : AndroidViewModel(application) {
+    //TODO rework to ViewModel when will be added Retrofit
+    private val mutableMovieListLiveData = MutableLiveData<List<Movie>>(emptyList())
+    private val mutableMovieLiveData = MutableLiveData<Movie>()
+    val moviesList: MutableLiveData<List<Movie>> get() = mutableMovieListLiveData
+    val movie: MutableLiveData<Movie> get() = mutableMovieLiveData
+
+    fun loadMoviesData() {
+        viewModelScope.launch {
+            moviesList.postValue(loadMovies(getApplication()))
+        }
+    }
+
+    fun loadSelectedMovie(bundle: Bundle?) {
+        movie.value = bundle?.getParcelable(selectedMovie)
+    }
+}
