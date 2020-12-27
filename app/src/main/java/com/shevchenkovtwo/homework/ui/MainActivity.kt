36394305021c3c4b60268d4c74af8fe8a308de55
vladimir4999 <shevchenkovtwo@gmail.com>
2020-12-27 17:38:47 +0300

    Add bottomNavigation

--- app/src/main/java/com/shevchenkovtwo/homework/ui/MainActivity.kt
+++ app/src/main/java/com/shevchenkovtwo/homework/ui/MainActivity.kt
@@ -1,22 +1,23 @@
-package com.shevchenkovtwo.homework
+package com.shevchenkovtwo.homework.ui
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
 import com.shevchenkovtwo.homework.databinding.ActivityMainBinding
 
 
 class MainActivity : AppCompatActivity() {
-
     private lateinit var mainActivityBinding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         mainActivityBinding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(mainActivityBinding.root)
+        val navView = mainActivityBinding.BottomNavigationView
         val navHostFragment =
-            supportFragmentManager.findFragmentById(mainActivityBinding.container.id) as NavHostFragment
+        supportFragmentManager.findFragmentById(mainActivityBinding.navHostFragment.id) as NavHostFragment
         val navController = navHostFragment.navController
+        navView.setupWithNavController(navController)
     }
-
 }
