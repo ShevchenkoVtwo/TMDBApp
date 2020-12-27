afb199ccd980b95b55c5b61db2062c4015888d29
vladimir4999 <shevchenkovtwo@gmail.com>
2020-12-27 17:33:36 +0300

    Add parcelable to data classes

--- app/src/main/java/com/shevchenkovtwo/homework/data/Genre.kt
+++ app/src/main/java/com/shevchenkovtwo/homework/data/Genre.kt
@@ -1,3 +1,8 @@
 package com.shevchenkovtwo.homework.data
 
-data class Genre(val id: Int, val name: String)
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+
+@Parcelize
+data class Genre(val id: Int, val name: String) : Parcelable
