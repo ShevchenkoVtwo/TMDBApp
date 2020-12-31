3d9f08f66b898bbfbe4c22ab15c53da1b6bfb381
vladimir4999 <shevchenkovtwo@gmail.com>
2020-12-31 13:24:03 +0300

    Fix nullable in loadSelectedMovie

--- app/src/main/java/com/shevchenkovtwo/homework/ui/viewmodles/MoviesViewModel.kt
+++ app/src/main/java/com/shevchenkovtwo/homework/ui/viewmodles/MoviesViewModel.kt
@@ -1,6 +1,5 @@
 package com.shevchenkovtwo.homework.ui.viewmodles
 
-
 import android.app.Application
 import android.os.Bundle
 import androidx.lifecycle.AndroidViewModel
@@ -10,6 +9,7 @@ import com.shevchenkovtwo.homework.data.Movie
 import com.shevchenkovtwo.homework.data.loadMovies
 import com.shevchenkovtwo.homework.ui.adapters.MoviesAdapter.Companion.selectedMovie
 import kotlinx.coroutines.launch
+
 
 class MoviesViewModel(application: Application) : AndroidViewModel(application) {
     //TODO rework to ViewModel when will be added Retrofit
@@ -26,7 +26,7 @@ class MoviesViewModel(application: Application) : AndroidViewModel(application) 
         }
     }
 
-    fun loadSelectedMovie(bundle: Bundle?) {
-        movie.value = bundle?.getParcelable(selectedMovie)
+    fun loadSelectedMovie(bundle: Bundle) {
+        movie.value = bundle.getParcelable(selectedMovie)
     }
 }
