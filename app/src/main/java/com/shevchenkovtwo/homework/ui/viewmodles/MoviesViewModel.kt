fe2e3b065e265db82fec28e154e1c14aec87c62a
vladimir4999 <shevchenkovtwo@gmail.com>
2021-01-04 16:55:12 +0300

    Fix mistypes

--- app/src/main/java/com/shevchenkovtwo/homework/ui/viewmodles/MoviesViewModel.kt
+++ app/src/main/java/com/shevchenkovtwo/homework/ui/viewmodles/MoviesViewModel.kt
@@ -3,6 +3,7 @@ package com.shevchenkovtwo.homework.ui.viewmodles
 import android.app.Application
 import android.os.Bundle
 import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.viewModelScope
 import com.shevchenkovtwo.homework.data.Movie
@@ -15,8 +16,8 @@ class MoviesViewModel(application: Application) : AndroidViewModel(application) 
     //TODO rework to ViewModel when will be added Retrofit
     private val mutableMovieListLiveData = MutableLiveData<List<Movie>>(emptyList())
     private val mutableMovieLiveData = MutableLiveData<Movie>()
-    val moviesList: MutableLiveData<List<Movie>> get() = mutableMovieListLiveData
-    val movie: MutableLiveData<Movie> get() = mutableMovieLiveData
+    val moviesList: LiveData<List<Movie>> get() = mutableMovieListLiveData
+    val movie: LiveData<Movie> get() = mutableMovieLiveData
 
     fun loadMoviesData() {
         if (mutableMovieListLiveData.value.isNullOrEmpty()) {
