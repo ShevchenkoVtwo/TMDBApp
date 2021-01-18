8772024fd106da4ce47ba1ac9979d9ed3252bcbd
vladimir4999 <shevchenkovtwo@gmail.com>
2021-01-18 10:24:48 +0300

    Rework Fragments

--- app/src/main/java/com/shevchenkovtwo/homework/ui/fragments/MovieDetailFragment.kt
+++ app/src/main/java/com/shevchenkovtwo/homework/ui/fragments/MovieDetailFragment.kt
@@ -1,28 +1,40 @@
 package com.shevchenkovtwo.homework.ui.fragments
 
+import android.content.ContentValues.TAG
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import coil.load
-import com.google.android.material.snackbar.Snackbar
 import com.shevchenkovtwo.homework.R
-import com.shevchenkovtwo.homework.data.Movie
+import com.shevchenkovtwo.homework.data.models.MovieDetails
 import com.shevchenkovtwo.homework.databinding.FragmentMovieDetailBinding
+import com.shevchenkovtwo.homework.network.ResponsesLogging
 import com.shevchenkovtwo.homework.ui.adapters.ActorsAdapter
+import com.shevchenkovtwo.homework.ui.adapters.MoviesAdapter.Companion.selectedMovie
 import com.shevchenkovtwo.homework.ui.adapters.calculateMovieRating
-import com.shevchenkovtwo.homework.ui.viewmodles.MoviesViewModel
+import com.shevchenkovtwo.homework.ui.viewmodels.MovieDetailsViewModel
+import com.shevchenkovtwo.homework.ui.viewmodels.MoviesViewModelFactory
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.launch
 
 
 class MovieDetailFragment : Fragment() {
 
     private var fragmentMovieDetailBinding: FragmentMovieDetailBinding? = null
-    private val viewModel: MoviesViewModel by viewModels()
+    private var job: Job? = null
+    private val viewModel: MovieDetailsViewModel by viewModels { MoviesViewModelFactory() }
+    private val adapter = ActorsAdapter()
+    private var movieDetails: MovieDetails? = null
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
         val binding = FragmentMovieDetailBinding.inflate(inflater, container, false)
@@ -32,40 +44,77 @@ class MovieDetailFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        arguments?.let { viewModel.loadSelectedMovie(it) }
-        viewModel.movie.value?.let{ movie ->
-            initViews(movie)
-            if (checkActorsList(movie))
-                Snackbar.make(view, getString(R.string.error_message), Snackbar.LENGTH_SHORT).show()
-        }
+        setClickListenerForNavigation()
+        movieDetails = arguments?.getParcelable(selectedMovie)
+        movieDetails?.let { initViews(it) }
+        initLoad()
+        initViewModel()
+        initRecyclerView()
     }
 
-    private fun initViews(movie: Movie) {
+    private fun setClickListenerForNavigation() {
         fragmentMovieDetailBinding?.apply {
-            movieName.text = movie.title
-            movieStoryline.text = movie.overview
-            movieRating.rating = movie.ratings.calculateMovieRating()
-            pg.text = getString(R.string.pg,movie.minimumAge)
-            reviews.text = getString(R.string.reviews,movie.numberOfRatings)
-            tag.text = movie.genres.joinToString { genre -> genre.name }
-            mask.load(movie.backdrop)
-            actors.let {
-                it.adapter = ActorsAdapter(movie.actors)
-                it.layoutManager =
-                    LinearLayoutManager(requireContext(), RecyclerView.HORIZONTAL, false)
-            }
             back.setOnClickListener {
                 findNavController().navigate(R.id.moviesListFragment)
             }
         }
     }
 
-    private fun checkActorsList(movie: Movie): Boolean {
-        return movie.actors.isNullOrEmpty()
+    private fun initLoad() {
+        movieDetails?.apply {
+            viewModel.loadActors(tmdbId)
+        }
+    }
+
+    private fun initViewModel() {
+        job?.cancel()
+        job = lifecycleScope.launch {
+            viewModel.stateFlow.collectLatest { response ->
+                when (response) {
+                    is ResponsesLogging.Success -> {
+                        adapter.differ.submitList(response.data)
+                        fragmentMovieDetailBinding?.loadingDataProgressBar?.isVisible = false
+                    }
+                    is ResponsesLogging.Loading -> {
+                        fragmentMovieDetailBinding?.loadingDataProgressBar?.isVisible = true
+                    }
+                    is ResponsesLogging.Error -> {
+                        fragmentMovieDetailBinding?.loadingDataProgressBar?.isVisible = false
+                        response.message.let {
+                            Log.e(TAG, "Something went wrong!")
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    private fun initViews(movie: MovieDetails) {
+        fragmentMovieDetailBinding?.apply {
+            movieName.text = movie.title
+            movieStoryline.text = movie.overview
+            movieRating.rating = movie.ratings.calculateMovieRating()
+            pg.text = getString(R.string.pg, if (movie.age) 16 else 13)
+            reviews.text = getString(R.string.reviews, movie.numberOfRatings)
+            tag.text = movie.genres.joinToString { genre -> genre.name }
+            mask.load(movie.backdrop)
+        }
+    }
+
+    private fun initRecyclerView() {
+        fragmentMovieDetailBinding?.apply {
+            actors.let {
+                it.adapter = adapter
+                it.layoutManager =
+                    LinearLayoutManager(requireContext(),RecyclerView.HORIZONTAL,false)
+            }
+        }
     }
 
     override fun onDestroyView() {
         fragmentMovieDetailBinding = null
+        movieDetails = null
+        job = null
         super.onDestroyView()
     }
 }
--- app/src/main/java/com/shevchenkovtwo/homework/ui/fragments/MoviesListFragment.kt
+++ app/src/main/java/com/shevchenkovtwo/homework/ui/fragments/MoviesListFragment.kt
@@ -6,19 +6,28 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.core.view.isVisible
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
+import androidx.paging.LoadState
 import androidx.recyclerview.widget.GridLayoutManager
-import androidx.recyclerview.widget.RecyclerView
 import com.shevchenkovtwo.homework.databinding.FragmentMoviesListBinding
 import com.shevchenkovtwo.homework.ui.adapters.MoviesAdapter
-import com.shevchenkovtwo.homework.ui.viewmodles.MoviesViewModel
+import com.shevchenkovtwo.homework.ui.viewmodels.MoviesViewModel
+import com.shevchenkovtwo.homework.ui.viewmodels.MoviesViewModelFactory
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.launch
 
 
 class MoviesListFragment : Fragment() {
 
     private var fragmentMoviesListBinding: FragmentMoviesListBinding? = null
-    private val moviesListMoviesViewModel: MoviesViewModel by viewModels()
     private var spanCount: Int = 2
+    private var job: Job? = null
+    private val viewModel: MoviesViewModel by viewModels { MoviesViewModelFactory() }
+    private val adapter = MoviesAdapter()
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
         val binding = FragmentMoviesListBinding.inflate(inflater, container, false)
@@ -30,22 +39,48 @@ class MoviesListFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        initViews()
+        initViewModel()
+        initAdapter()
+        initRecyclerView()
     }
 
-    private fun initViews() {
-        moviesListMoviesViewModel.loadMoviesData()
-        moviesListMoviesViewModel.moviesList.observe(viewLifecycleOwner) { movies ->
-            fragmentMoviesListBinding?.moviesList?.let { recyclerView ->
-                recyclerView.adapter = MoviesAdapter(movies)
-                recyclerView.layoutManager =
-                    GridLayoutManager(requireContext(), spanCount, RecyclerView.VERTICAL, false)
+    private fun initViewModel() {
+        job?.cancel()
+        job = lifecycleScope.launch {
+            viewModel.loadNowPlayingMovies().collectLatest {
+                adapter.submitData(it)
+            }
+        }
+    }
+
+    private fun initRecyclerView() {
+        fragmentMoviesListBinding?.moviesList?.apply {
+            layoutManager =
+                GridLayoutManager(requireContext(), spanCount)
+        }
+    }
+
+    private fun initAdapter() {
+        fragmentMoviesListBinding?.apply {
+            moviesList.adapter = adapter
+            adapter.addLoadStateListener { loadState ->
+                progressBar.isVisible = loadState.source.refresh is LoadState.Loading
+                loadingDataProgressBar.isVisible = loadState.append is LoadState.Loading
+
+                val errorState = loadState.source.append as? LoadState.Error
+                    ?: loadState.source.prepend as? LoadState.Error
+                    ?: loadState.append as? LoadState.Error
+                    ?: loadState.prepend as? LoadState.Error
+                errorState?.let {
+                    Toast.makeText(requireContext(), "${it.error}", Toast.LENGTH_LONG).show()
+                }
             }
         }
     }
 
     override fun onDestroyView() {
         fragmentMoviesListBinding = null
+        job = null
         super.onDestroyView()
     }
 
