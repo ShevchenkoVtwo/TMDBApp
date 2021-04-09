aef82f9107b75cf87d135864875e22e5fa0ac29c
vladimir4999 <shevchenkovtwo@gmail.com>
2021-04-09 12:37:41 +0300

    Delete unnecessary files and update manifest

--- app/src/main/java/com/shevchenkovtwo/homework/data/models/Configuration.kt
+++
@@ -1,9 +0,0 @@
-package com.shevchenkovtwo.homework.data.models
-
-import com.squareup.moshi.Json
-
-
-data class Configuration(
-    @Json(name = "secure_base_url")
-    val imagesSecureUrl: String,
-)
--- app/src/main/java/com/shevchenkovtwo/homework/data/models/MoviesId.kt
+++
@@ -1,8 +0,0 @@
-package com.shevchenkovtwo.homework.data.models
-
-import com.squareup.moshi.Json
-
-
-data class MoviesId(
-    @Json(name = "id")
-    val tmdbId: Int)
