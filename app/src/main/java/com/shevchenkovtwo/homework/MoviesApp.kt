0b985d8da7c487e089af211f7c3d614d43e62777
Vladimir <shevchenkovtwo@gmail.com>
2021-09-27 14:08:58 +0300

    Remove and delete files

--- app/src/main/java/com/shevchenkovtwo/homework/MoviesApp.kt
+++
@@ -1,9 +0,0 @@
-package com.shevchenkovtwo.homework
-
-import android.app.Application
-import dagger.hilt.android.HiltAndroidApp
-import kotlinx.coroutines.GlobalScope
-
-
-@HiltAndroidApp
-class MoviesApp : Application()
